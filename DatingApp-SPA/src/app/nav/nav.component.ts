f67fa47d106f1bd827d7d86a3fd10cde10fcf73a
rajesh <rajeshmettur@gmail.com>
2020-01-03 18:30:14 +0530

    End of Session 5

---
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+import { AlertifyjsService } from '../_services/alertifyjs.service';
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+  model: any = {};
+
+  constructor(public authService: AuthService, private alertify: AlertifyjsService) { }
+
+  ngOnInit() {
+  }
+
+  login() {
+    this.authService.login(this.model).subscribe(
+      next => {
+        this.alertify.success('logged in successfully');
+      },
+      error => {
+        this.alertify.error(error);
+      }
+    );
+  }
+
+  loggedIn() {
+   return this.authService.loggedIn();
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    this.alertify.warning('logout');
+  }
+}
---
+++ DatingApp.API/Helper/Extensions.cs
@@ -0,0 +1,18 @@
+using System;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace DatingApp.API.Helper
+{
+    public static class Extensions
+    {
+        public static void AddApplicationError(this HttpResponse response, string message)
+        {
+            response.Headers.Add("Application-Error", message);
+            response.Headers.Add("Access-Control-Expose-Headers", "Application-Error");
+            response.Headers.Add("Access-Control-Allow-Origin", "*");
+        }
+
+    }
+}
